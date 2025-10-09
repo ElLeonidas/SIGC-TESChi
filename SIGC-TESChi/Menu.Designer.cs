d8903711526d0b961311906b2a597fa5730d6c59
DANIEL VELAZQUEZ TREJO <2021452168@teschi.edu.mx>
2025-10-09 13:16:19 -0600

    0.1.1 vercion temprana del menu
    espero que el dise;o salga bien ayuda aimep3

--- SIGC-TESChi/Menu.Designer.cs
+++ SIGC-TESChi/Menu.Designer.cs
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MenuControl = new System.Windows.Forms.Panel();
+            this.Categorias = new System.Windows.Forms.Panel();
+            this.MenuControl.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // MenuControl
+            // 
+            this.MenuControl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MenuControl.Controls.Add(this.Categorias);
+            this.MenuControl.Location = new System.Drawing.Point(1, 1);
+            this.MenuControl.Name = "MenuControl";
+            this.MenuControl.Size = new System.Drawing.Size(1879, 1052);
+            this.MenuControl.TabIndex = 0;
+            // 
+            // Categorias
+            // 
+            this.Categorias.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Categorias.Location = new System.Drawing.Point(206, 45);
+            this.Categorias.Name = "Categorias";
+            this.Categorias.Size = new System.Drawing.Size(1672, 1006);
+            this.Categorias.TabIndex = 0;
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1882, 1055);
+            this.Controls.Add(this.MenuControl);
             this.Name = "Menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
+            this.MenuControl.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel MenuControl;
+        private System.Windows.Forms.Panel Categorias;
     }
 }
